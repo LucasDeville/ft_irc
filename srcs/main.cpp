e6191e270d1e3bdb7f1d9802cea43f17f5497259
Lulu <lu.ca.deville@outlook.fr>
2023-12-12 15:43:12 +0100

    server main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,21 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bpleutin <bpleutin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ldeville <ldeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 10:33:17 by ldeville          #+#    #+#             */
-/*   Updated: 2023/12/12 11:22:58 by bpleutin         ###   ########.fr       */
+/*   Updated: 2023/12/12 15:42:18 by ldeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.hpp"
+#include "Server.hpp"
 
 int	main(int argc, char **argv) {
 
-    if (argc != 3 && atoi(argv[2]) > 65535 && atoi(argv[2]) < 0)
+    if (argc != 3 || (atoi(argv[1]) > 65535 || atoi(argv[1]) < 1))
     {
         std::cout << "Error: could not start IRC." << std::endl;
         return (-1);
     }
+    Server server(atoi(argv[1]), argv[2]);
     return (0);
 }
