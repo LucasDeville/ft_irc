cc4980be1c14f3f61df7d1e1770bed1d8cb1ca42
Lucas Deville <ldeville@c2r7p5.42perpignan.fr>
2023-12-13 13:51:17 +0100

    Exception

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: ldeville <ldeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 14:43:05 by ldeville          #+#    #+#             */
-/*   Updated: 2023/12/12 14:52:41 by ldeville         ###   ########.fr       */
+/*   Updated: 2023/12/13 12:14:13 by ldeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ private:
 
 	std::string	_username;
 	std::string	_nickname;
+	std::string	_name;
+	std::string	_passwd;
+	int			_mode;
 
 public:
 
@@ -29,6 +32,9 @@ public:
 
 	std::string const &	getUsername() const { return _username; };
 	std::string const &	getNickname() const { return _nickname; };
+	std::string const &	getName() const { return _name; };
+	std::string const &	getPassword() const { return _passwd; };
+	int const &			getMode() const { return _mode; };
 };
 
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldeville <ldeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 10:33:17 by ldeville          #+#    #+#             */
-/*   Updated: 2023/12/12 15:42:18 by ldeville         ###   ########.fr       */
+/*   Updated: 2023/12/13 12:12:13 by ldeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,11 @@ int	main(int argc, char **argv) {
         return (-1);
     }
     Server server(atoi(argv[1]), argv[2]);
+    try {
+        server.createServer();
+    }
+    catch (const std::exception &e){
+        std::cout << e.what() << std::endl;
+    }
     return (0);
 }
