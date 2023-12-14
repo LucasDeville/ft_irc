2d282d9817ef3dad0f7ae65ecc3915f767f222b9
Thawnder <realthawnder@outlook.fr>
2023-12-14 12:41:50 +0100

    recovered main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bpleutin <bpleutin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 10:33:17 by ldeville          #+#    #+#             */
-/*   Updated: 2023/12/13 11:35:01 by bpleutin         ###   ########.fr       */
+/*   Updated: 2023/12/14 12:41:41 by bpleutin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 
 int	main(int argc, char **argv) {
 
-    if (argc != 3 || (atoi(argv[1]) > 65535 || atoi(argv[1]) < 1024))
+    if (argc != 3 || (atoi(argv[1]) > 65535 || atoi(argv[1]) < 1))
     {
         std::cout << "Error: could not start IRC." << std::endl;
         return (-1);
     }
     Server server(atoi(argv[1]), argv[2]);
+    try {
+        server.createServer();
+    }
+    catch (const std::exception &e){
+        std::cout << e.what() << std::endl;
+    }
     return (0);
 }
